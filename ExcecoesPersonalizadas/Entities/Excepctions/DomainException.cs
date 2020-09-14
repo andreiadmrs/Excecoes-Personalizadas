795a7685607a3099e8cc0992606da5682093c1d6
Andreia de Moraes <andreiadmrs@gmail.com>
2020-09-14 16:38:07 -0300

    Exercício do curso de C#. Exceções personalizadas.

---
+++ ExcecoesPersonalizadas/Entities/Excepctions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExcecoesPersonalizadas.Entities.Excepctions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {            
+        }
+    }
+}
---
+++ ExcecoesPersonalizadas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using ExcecoesPersonalizadas.Entities;
+using ExcecoesPersonalizadas.Entities.Excepctions;
+
+namespace ExcecoesPersonalizadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter account data: ");
+            Console.WriteLine("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Holder: ");
+            string holder = Console.ReadLine();
+            Console.WriteLine("Initional Balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine("Enter amount to withdraw: ");
+            double widthdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                account.Withdraw(widthdraw);
+
+                Console.WriteLine(account);
+            }
+
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
+
